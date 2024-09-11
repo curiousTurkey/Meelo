312226dd84bf8c7bae1be774128422be02ccfa44
Eldhose Babu <eldhosebabu197@gmail.com>
2024-09-11 11:22:32 -0400

    Disabled input, local and output Flags
    Disabled input flags IXON, ICRNL
    Disabled output flags OPOST
    Disabled local flags ECHO, ICANON, IEXTEN, ISIG
    
    Printed the input characters with their ASCII Values

--- meelo.c
+++ meelo.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 //COOKED MODE : The input is only sent to the program when the user presses "Enter" key. (also called canonical mode)
@@ -11,13 +13,22 @@ void enableRawMode(){
     atexit(disableRawMode);
     tcgetattr(STDIN_FILENO, &original_termios); // gets the current attributes of the terminal settings
     raw = original_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main(){
     enableRawMode();
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); // quits when the 'q' character is read.
+    char c; 
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    } 
+    // quits when the 'q' character is read.
     // read function takes 3 args. an int type args which is stdin_fileno, stdout_fileno and error,
     //second argument is the void *buf which is the buffer to store data (address of the variable to store data)
     //third argument is the size_t that defines the number of bytes to read.
