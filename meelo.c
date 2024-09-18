3098d13fc4e83ec79d254178052bd6c254dc728d
Eldhose Babu <eldhosebabu197@gmail.com>
2024-09-18 17:35:06 -0400

    die() and error handling
    implemented die() function to quit out of program when an error occurs.
    error handling is done through perror which comes from errno.h header file

--- meelo.c
+++ meelo.c
@@ -1,31 +1,47 @@
 #include <unistd.h>
 #include <ctype.h>
+#include<errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 //COOKED MODE : The input is only sent to the program when the user presses "Enter" key. (also called canonical mode)
 struct termios original_termios;
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1){
+        die("tcsetattr");
+    }
+}
+void die(char *s){
+   perror(s);
+   ecit(1); 
 }
 void enableRawMode(){
     struct termios raw; //this declares a raw struct that stores terminal settings
+    if(tcgetattr(STDIN_FILENO, &original_termios) == -1) die("tcgetattr");// gets the current attributes of the terminal settings
     atexit(disableRawMode);
-    tcgetattr(STDIN_FILENO, &original_termios); // gets the current attributes of the terminal settings
+     
     raw = original_termios;
-    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | ISTRIP | INPCK);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 10;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 int main(){
     enableRawMode();
     char c; 
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c)){
             printf("%d\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == 'q'){
+            break;
         }
     } 
     // quits when the 'q' character is read.
